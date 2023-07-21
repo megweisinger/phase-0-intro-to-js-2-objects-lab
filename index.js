f03bcdd14e2aad8bf9404a3a56b897db3770f280
Meggie Weisinger <meggie.weisinger@outlook.com>
2023-07-21 15:01:10 -0500

    7 passing test

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+const employee = {
+    name: "Meg",
+    streetAddress: "12 Aste",
+};
+employee
+function updateEmployeeWithKeyAndValue (employee, key, value){
+    return {...employee,...{ [key]: value}}
+};
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+function deleteFromEmployeeByKey (employee, key, value){
+    const newObject = {...employee};
+    delete newObject[key];
+    return newObject;
+}
+function destructivelyDeleteFromEmployeeByKey (employee, key, value) {
+    delete employee[key];
+    return employee
+}
